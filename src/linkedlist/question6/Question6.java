6876f27c9068aa496fb309fa829aeceb771d17ef
iceberg-work <iceberg-work@qq.com>
2022-02-11 23:47:12 +0800

    新建了Question6

---
+++ src/linkedlist/question6/Question6.java
@@ -0,0 +1,29 @@
+package linkedlist.question6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question6 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ListNode {
+    int val;
+     ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Solution {
+
+    public int[] reversePrint(ListNode head) {
+        if (head.next == null) {
+            return new int[]{head.val};
+        }
+        int[] arr0 = reversePrint(head.next);
+        //int[] arr1 = ;
+        //int[] arr2 = new int[arr0.length +arr1.length];
+        return new int[]{head.val};
+    }
+}
